230b6db1d80688d4f2063fa1478d991808655a71
Efim Trofimov <grek521@mail.ru>
2022-04-07 00:50:00 +0300

    feat: finished the lab2

--- lab2/main.py
+++ lab2/main.py
@@ -1,21 +1,35 @@
 import pandas as pd
-from requestsData import request_students_data
+from requestsData import request_students_in_data_frame
+from requestsData import request_students
 
-student_data_frame = request_students_data()
+def search_the_best_course_by_gpa():
+  course_average = request_students()
+  print(course_average)
+  sorted_course_average = sorted(course_average, key=lambda item: item[list(item.keys())[0]], reverse=True)
 
-def setup(data): 
-  pd.set_option('display.max_columns', None)
-  data.head()
+  print('\nThe best course  by medium score:')
+  print(sorted_course_average[0])
+
+search_the_best_course_by_gpa()
+
+### <---
+
+student_data_frame = request_students_in_data_frame()
+
+### <---
 
 def save_to_csv(data: pd.DataFrame):
   data.to_csv('data.csv')
 
+### <---
+
 def search_mid_value(data: pd.DataFrame):
-  print("Введите среднее число")
+  print("Enter the grade mid")
   value_for_search = float(input())
   student_id = data["gradeMid"].sub(value_for_search).abs().idxmin()
   print(data.iloc[[student_id]])
 
-setup(student_data_frame)
+### <---
+
 save_to_csv(student_data_frame)
 search_mid_value(student_data_frame)
--- lab2/requestsData.py
+++ lab2/requestsData.py
@@ -5,7 +5,24 @@ from Student import Student
 
 urls = ['https://www.hse.ru/n/student-ratings/api?unit=122392301&course=1&from=576252978', 'https://www.hse.ru/n/student-ratings/api?unit=122392301&course=2&from=576252978', 'https://www.hse.ru/n/student-ratings/api?unit=122392301&course=3&from=576252978', 'https://www.hse.ru/n/student-ratings/api?unit=122392301&course=4&from=576252978']
 
-def request_students_data():
+
+def request_students():
+  sum_of_gpa_each_course = []
+  for url in urls:
+    students_data = requests.get(url).json()
+    parsed_students_data = students_data["data"]
+    student_list = [Student(**s) for s in parsed_students_data]
+    #мог написать код ниже в одну строчку, но сильно ухудшалась читабельность
+    total_gpa = round(sum(list(map(lambda item: item.gpa, student_list))), 2)
+    sum_of_gpa_each_course.append(total_gpa)
+
+  
+  sum_of_gpa_for_each_course = [{x + 1: y} for x, y in enumerate(sum_of_gpa_each_course)]
+  return sum_of_gpa_for_each_course
+
+#### <----
+
+def request_students_in_data_frame():
   dataframe = []
   for url in urls:
     students_data = requests.get(url).json()
@@ -14,6 +31,4 @@ def request_students_data():
     dataframe.append(pd.DataFrame(data=student_list))
 
   student_data_frame = pd.concat(dataframe, ignore_index=True)
-  
   return student_data_frame
-
